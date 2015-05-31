0e299ee50e574d1b05c9228331147a59acfd21d4
heartbeatsfeeling <499954367@qq.com>
2015-05-31 16:34:51 +0800

    add frame

---
+++ example/validator.js
@@ -0,0 +1,24 @@
+/*validator({
+	id:"na",
+	tipPlacement:function(element,text){
+
+	},
+	rules:{
+		'a':{
+			tipPlacement:function(element,text){
+
+			},
+			wrong:"输入的错误",
+			right:"正确",
+			empty:'为空',
+			focus:"请您输入",
+			required:true,//根据元素判断type ，确定click blur change 
+			//handler:'core',
+			limit:/\d/
+		}
+	}
+});
+validator.get('na').result();//验证结果*/
+;(function($){
+
+})(jQuery);
