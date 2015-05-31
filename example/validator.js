b542e00088cf59596921b876e3121fe553c7e3c3
heartbeatsfeeling <499954367@qq.com>
2015-05-31 19:26:41 +0800

    初步框架

--- example/validator.js
+++ example/validator.js
@@ -20,5 +20,20 @@
 });
 validator.get('na').result();//验证结果*/
 ;(function($){
-
+	var validator=function(config){
+		return new  validator.prototype.init(config);
+	};
+	validator.fn=validator.prototype={
+		init:function(config){
+			console.log(config)
+		},
+		var validRule = { //验证规则
+			email: /^[\w.-]+?@[a-z0-9]+?\.[a-z]{2,6}$/i, //电子邮件
+			idnumber: /^\d{6}(?:((?:19|20)\d{2})(?:0[1-9]|1[0-2])(?:0[1-9]|[1-2]\d|3[0-1])\d{3}(?:x|X|\d)|(?:\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1-2]\d|3[0-1])\d{3}))$/, //身份证
+			phone: /^\d{11}$/, //手机号
+			empty: /[^\s]{1,}/ //非空
+		}
+	};
+	validator.prototype.init.prototype=validator.fn;
+	window['validator']=validator;
 })(jQuery);
