93beb28011b7f7c86474fe672bb0a64d21967699
heartbeatsfeeling <499954367@qq.com>
2015-06-16 15:58:17 +0800

    new api

--- example/validator.js
+++ example/validator.js
@@ -13,46 +13,39 @@
 			right:"正确",
 			empty:'为空',
 			focus:"请您输入",
-			required:true,//根据元素判断type ，确定click blur change 
-			limit:/\d/ [string/regexp]
+			required:true,
+			limit:/\d/
 		}
 	}
 });
 validator.get('na').valid('a');//单独验证a
 validator.get('na').valid()//.result();//验证全部
-validator.get('na').trigger('a','error','出错了',tipPlacement);//触发a error
+validator.get('na').triggerValid('a','error','出错了',tipPlacement);//触发a error
 */
-;(function($){
-	var validator=function(config){
-		return new  validator.prototype.init(config);
+;
+(function($) {
+	var validator = function(config) {
+		validator.rules[config.id] = config.rules;
 	};
-	validator.fn=validator.prototype={
-		init:function(config){//初始化
-			this[config.id]=this.api;
-		},
-		get:function(id){//获取验证id
-			return this[id]
-		},
-		api:function(){//对外接口
+	validator.rules={};
+	validator.list = {};
+	validator.get = function(id) {
+		return validator.list[id] = new create(id);
+	};
+	var create = function(id) {
+		this.element=validator.rules[id];
+	};
+	create.prototype = {
+		valid: function(id) {
 
 		},
-		addMethod:function(){
+		triggerValid: function(id,type,message,tipPlacement) {
 
-		},
-		triggerValid:function(){
-
-		},
-		validRexp : { //验证规则
-			isEmail: /^[\w.-]+?@[a-z0-9]+?\.[a-z]{2,6}$/i, //电子邮件
-			isIdnumber: /^\d{6}(?:((?:19|20)\d{2})(?:0[1-9]|1[0-2])(?:0[1-9]|[1-2]\d|3[0-1])\d{3}(?:x|X|\d)|(?:\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1-2]\d|3[0-1])\d{3}))$/, //身份证
-			isPhone: /^(\+?0?86\-?)?1[345789]\d{9}$/, //手机号
-			isEmpty: /[^\s]{1,}/ //非空
 		}
 	};
-	validator.prototype.init.prototype=validator.fn;
 	if (typeof exports !== 'undefined') {
 		module.exports = validator
 	} else {
-		window['validator']=validator;
+		window['validator'] = validator;
 	};
 })(jQuery);
