c5fb27f133ea42cf7b07fee42ecbab62eb283b12
heartbeatsfeeling <499954367@qq.com>
2015-06-17 22:12:38 +0800

    new api

--- example/validator.js
+++ example/validator.js
@@ -1,7 +1,7 @@
 /*validator({
 	id:"na",
 	tipPlacement:function(element,text){
-
+		defaultValue:"请输入文字",
 	},
 	rules:{
 		'a':{
@@ -24,25 +24,68 @@ validator.get('na').triggerValid('a','error','出错了',tipPlacement);//触发a
 */
 ;
 (function($) {
+	var $d=$(document);
 	var rules={};
 	var list = {};
 	var total={};
+	var configTipPlacement=null;
 	var validator = function(config) {
 		var rule=config.rules;
 		$.each(rule,function(key,item){
-			//
+			$d.on('focus','#'+key,function(){
+				_focus($(this),item);
+			});
+			$d.on('blur','#'+key,function(){
+				_blur($(this),item);
+			});
 			total[key]=item;
-		});
+		})
+		configTipPlacement=config.tipPlacement;
 		rules[config.id] = config.rules;
 	};
 	validator.get = function(id) {
 		return list[id] = new create(id);
 	};
-	var _focus=function(){
-
+	var _focus=function($this,options){
+		var tipPlacement=options.tipPlacement||configTipPlacement;
+		var focusText=options.focus;
+		if(focusText){
+			tipPlacement($this,"<div class='validator-focus'>"+focusText+"</div>");
+		};
 	};
-	var _blur=function(){
-
+	var _blur=function($this,options){
+		var tipPlacement=options.tipPlacement||configTipPlacement;
+		var value=$.trim($this.val());
+		var limit=options.limit;
+		var wrongText=options.wrong||"错误";
+		var emptyText=options.empty||"为空";
+		if(options.required){//必写
+			if(value){
+				if( limit.test(value) ){
+					tipPlacement($this,"");
+					return true;
+				}else{
+					tipPlacement($this,"<div class='validator-blur'>"+wrongText+"</div>");
+					return false;
+				}
+			}else{
+				tipPlacement($this,"<div class='validator-blur'>"+emptyText+"</div>");
+				return false;
+			}
+		}else{
+			if(value){
+				if( limit.test(value) ){
+					tipPlacement($this,"");
+					return true;
+				}else{
+					tipPlacement($this,"<div class='validator-blur'>"+wrongText+"</div>");
+					return false;
+				}
+			}else{
+				tipPlacement($this,"");
+				return true;
+			}
+		}
 	};
 	var create = function(id) {
 		this.element=rules[id];
