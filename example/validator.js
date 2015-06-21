fc5aa6d14ff223cb100a98029e432c48343ea12f
heartbeatsfeeling <499954367@qq.com>
2015-06-21 16:46:58 +0800

    new api

--- example/validator.js
+++ example/validator.js
@@ -1,114 +1,134 @@
-/*validator({
+/*
+validator({
 	id:"na",
-	tipPlacement:function(element,text){
-		defaultValue:"请输入文字",
-	},
+	tipPlacement:function(element,tip){
+			element.closest('.common').find('.tip').html(tip);
+		},
 	rules:{
 		'a':{
-			tipPlacement:function(element,text){
-
-			},
-			defaultValue:"请输入文字",
+			defaultValue:"请输入a",
 			wrong:"输入的错误",
 			right:"正确",
 			empty:'为空',
 			focus:"请您输入",
 			required:true,
-			limit:/\d/
+			limit:/^\d+$/
+		},
+		'b':{
+			defaultValue:"请输入b",
+			wrong:"输入的错误",
+			right:"正确",
+			empty:'为空',
+			focus:"请您输入",
+			required:true,
+			limit:/^\d+$/
 		}
 	}
 });
 validator.get('na').valid('a');//单独验证a
 validator.get('na').valid()//.result();//验证全部
 validator.get('na').triggerValid('a','error','出错了',tipPlacement);//触发a error
+节点隐藏，返回真 
+为空的处理 有点不对。。
 */
 ;
 (function($) {
-	var $d=$(document);
-	var rules={};
+	var rules = {};
 	var list = {};
-	var total={};
-	var configTipPlacement=null;
+	var total = {};
+	var configTipPlacement = null;
+	var defaultText = {
+		empty: '不能为空',
+		wrong: "输入有错误",
+		right: "",
+		focus: ""
+	};
 	var validator = function(config) {
-		var rule=config.rules;
-		$.each(rule,function(key,item){
-			$d.on('focus','#'+key,function(){
-				_focus($(this),item);
+		var rule = config.rules;
+		$.each(rule, function(key, item) {
+			var $element = $('#' + key);
+			item.element = $element;
+			$element.on('focus', function() {
+				_focus(item);
 			});
-			$d.on('blur','#'+key,function(){
-				_blur($(this),item);
+			$element.on('blur', function() {
+				_blur(item);
 			});
-			total[key]=item;
+			total[key] = item;
 		})
-		configTipPlacement=config.tipPlacement;
+		configTipPlacement = config.tipPlacement||noop;
 		rules[config.id] = config.rules;
 	};
 	validator.get = function(id) {
 		return list[id] = new create(id);
 	};
-	var _focus=function($this,options){
-		var tipPlacement=options.tipPlacement||configTipPlacement;
-		var focusText=options.focus;
-		if(focusText){
-			tipPlacement($this,"<div class='validator-focus'>"+focusText+"</div>");
+	var _focus = function(options) {
+		var tipPlacement = options.tipPlacement || configTipPlacement;
+		var focusText = options.focus || defaultText.focus;
+		if (focusText) {
+			tipPlacement(options.element, "<div class='validator-focus'>" + focusText + "</div>");
 		};
 	};
-	var _blur=function($this,options){
-		var tipPlacement=options.tipPlacement||configTipPlacement;
-		var value=$.trim($this.val());
-		var limit=options.limit;
-		var wrongText=options.wrong||"错误";
-		var emptyText=options.empty||"为空";
-		if(options.required){//必写
-			if(value){
-				if( limit.test(value) ){
-					tipPlacement($this,"");
-					return true;
-				}else{
-					tipPlacement($this,"<div class='validator-blur'>"+wrongText+"</div>");
-					return false;
-				}
-			}else{
-				tipPlacement($this,"<div class='validator-blur'>"+emptyText+"</div>");
+	var _blur = function(options) {
+		return _validCore(options);
+	};
+	var _validCore = function(options) { //核心验证方法
+		var $element = options.element;
+		var value = $.trim($element.val());
+		var limit = options.limit;
+		var wrongText = options.wrong || defaultText.wrong;
+		var emptyText = options.empty || defaultText.empty;
+		var tipPlacement = options.tipPlacement || configTipPlacement;
+		if (!value || value === options.defaultValue) { //为空
+			if (options.required) { //必填
+				tipPlacement($element, "<div class='validator-blur'>" + emptyText + "</div>");
 				return false;
-			}
-		}else{
-			if(value){
-				if( limit.test(value) ){
-					tipPlacement($this,"");
-					return true;
-				}else{
-					tipPlacement($this,"<div class='validator-blur'>"+wrongText+"</div>");
-					return false;
-				}
-			}else{
-				tipPlacement($this,"");
+			} else {
+				tipPlacement($element, "");
 				return true;
 			}
-		}
+		} else {
+			if (options.required) {
+				if (limit.test(value)) {
+					tipPlacement($element, "");
+					return true;
+				} else {
+					tipPlacement($element, "<div class='validator-blur'>" + wrongText + "</div>");
+					return false;
+				}
+			} else {
+				if (limit.test(value)) {
+					tipPlacement($element, "");
+					return true;
+				} else {
+					tipPlacement($element, "<div class='validator-blur'>" + wrongText + "</div>");
+					return false;
+				}
+			}
+		};
 	};
+	var noop=function(){ };
 	var create = function(id) {
-		this.element=rules[id];
+		this.element = rules[id];
 	};
 	create.prototype = {
 		valid: function(id) {
-			var _this=this;
-			var result=true;
-			if(id){
-				result= _this.validHander(id)
-			}else{
-				$.each(this.element,function(id,item){
-					result=_this.validHander(id)?result:false;
+			var _this = this;
+			var result = true;
+			if (id) {
+				result = _this.validHander(id)
+			} else {
+				$.each(this.element, function(id, item) {
+					result = _this.validHander(id) ? result : false;
 				});
 			};
 			return result;
 		},
-		validHander:function(id){
-			var $this=$('#'+id);
-			var options=total[id];
-			return _blur($this,options)
+		validHander: function(id) {
+			var options = total[id];
+			return _blur(options)
 		},
-		triggerValid: function(id,type,message,tipPlacement) {
+		triggerValid: function(id, type, message, tipPlacement) {
 			//todo
 		}
 	};
